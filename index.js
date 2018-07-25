58d30a2feb73af2bceb5271c5fa0fbd9acea2b06
Tyler Soo Hoo <tyler.soohoo@gmail.com>
2018-07-25 21:44:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function add2(n) {
   return n + two
 }
 
-var funkyFunction = function functionName() {
-  return function functionName() {
+var funkyFunction = function() {
+  return function() {
     return "FUNKY!"
   }
 }
